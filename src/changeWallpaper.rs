c90c0f443374aeb6b4060184bf7ec9e8edfcfdfb
Jorge Ramírez <ing.jorgeivanramirezh@gmail.com>
2018-09-19 15:32:56 -0500

    add the validation to check the folder exists to store the image (#6)
    * add the validation to check the folder exists to store the image
    
    * Remove the iten[0].url
    
    remove the url fron the jq parameters because the new json have
    the whole url in the image settings

--- src/changeWallpaper.rs
+++ src/changeWallpaper.rs
@@ -3,14 +3,22 @@ use std::process::{Command, Stdio};
 use std::env;
 use std::str;
 use std::string::String;
+use std::fs;
+use std::path::Path;
 
-
-const IMAGE_PATH: &'static str = "/.natgeo-img/photo-of-the-day.jpg";
+const IMAGE_FOLDER: &'static str = "/.natgeo-img";
+const IMAGE_PATH: &'static str = "/photo-of-the-day.jpg";
 const NATGEO_JSON: &'static str = "https://www.nationalgeographic.com/photography/photo-of-the-day/_jcr_content/.gallery.json";
-const JQ_ARGUMENT: &'static str = ".items[0].url + .items[0].sizes[\"2048\"]";
+const JQ_ARGUMENT: &'static str = ".items[0].sizes[\"2048\"]";
 
 fn main() {
     let mut image_path = env::var("HOME").expect("no se pudo leer");
+
+    image_path.push_str(IMAGE_FOLDER);
+    if !Path::new(&image_path).exists() {
+        fs::create_dir(image_path.to_string()).expect("error creando el directorio"); 
+    }
+
     image_path.push_str(IMAGE_PATH);
     let owned_image_path = image_path.to_string();
     download_image(&owned_image_path);
